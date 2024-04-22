c3ef545744cef575ce5d68723cb58d65dafe9cee
rostikсheremys <rostikcheremys@gmail.com>
2024-04-22 14:39:53 +0300

    Initial commit

---
+++ Program/HorseRace.cs
@@ -0,0 +1,146 @@
+﻿using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Color = System.Windows.Media.Color;
+
+namespace Program
+{
+	class HorseRace
+	{
+		private Random _random = new();
+
+		public Image HorseImage { get; set; }
+
+		public Image JockeyImage { get; set; }
+
+		public string Name { get; private set; }
+
+		public Color Color { get; private set; }
+
+		private TimeSpan _time;
+
+		public TimeSpan Time 
+		{
+			get => _time;
+			set
+			{
+				_time = value;
+				Finished = true;
+			}
+		}
+
+		public int Speed { get; private set; }
+
+		public int CurrentPosition { get; set; }
+
+		public int PositionX { get; private set; }
+
+		public int PositionY { get; private set; }
+
+		private int _animationFrame;
+
+		public int AnimationFrame
+		{
+			get => _animationFrame;
+			set => _animationFrame = (value % 8);
+		}
+
+		public bool IsBidClosed { get; set; }
+
+		private double _money;
+
+        public double Money 
+		{
+			get => _money;
+			set
+			{
+				_money = Math.Round(value * Coefficient, 2);
+				IsBidClosed = true;
+			} 
+		}
+
+		private double _coefficient;
+
+        public double Coefficient 
+		{
+			get => _coefficient; 
+
+			set 
+			{
+				_coefficient = value;
+				if (_coefficient <= 1) IsBidClosed = true;
+			}
+		}
+
+		private bool _finished;
+
+		public bool Finished 
+		{
+			get => _finished;
+			private set 
+			{
+				_finished = value;
+				if (value)
+				{
+					IsBidClosed = true;
+					if (CurrentPosition == 1) ChangeBalance(Money * Coefficient);
+				}
+			} 
+		}
+
+		public Action<double> ChangeBalance;
+
+		public HorseRace(string name, Color color, int x, int y, Action<double> changeBalance)
+		{
+			Name = name;
+			Color = color;
+			IsBidClosed = false;
+			ChangeBalance += changeBalance;
+
+			Speed = _random.Next(3, 7);
+			Coefficient = 1.7 - Speed / 10.0;
+			AnimationFrame = 0;
+
+			HorseImage = new Image
+			{
+				Source = new BitmapImage(new Uri("Images/Horses/WithOutBorder_0000.png", UriKind.Relative)),
+				Width = 100,
+				Tag = "tmp"
+			};
+
+			JockeyImage = new Image
+			{
+				Source = new BitmapImage(new Uri($"Images/HorsesMask/mask_0000.png", UriKind.Relative)),
+				Width = 50,
+				Tag = "tmp"
+			};
+
+			PositionX = x;
+			PositionY = y;
+		}
+
+		public async Task MoveAsync()
+		{
+			int distance = await Task.Run(() => (int)(Speed * (_random.Next(4, 10) / 10.0)));
+			PositionX += distance;
+
+			AnimationFrame++;
+
+			var fileNumber = _animationFrame.ToString().Length > 1 ? _animationFrame.ToString() : "0" + _animationFrame.ToString();
+
+			HorseImage.Source = new BitmapImage(new Uri($"Images/Horses/WithOutBorder_00{fileNumber}.png", UriKind.Relative));
+			JockeyImage.Source = new BitmapImage(new Uri($"Images/HorsesMask/mask_00{fileNumber}.png", UriKind.Relative));
+
+			if (!IsBidClosed)
+			{
+				CalculateCoefficient();
+			}
+		}
+
+		private void CalculateCoefficient()
+		{
+			Coefficient = Math.Round(1.7 - Speed / 10.0 + CurrentPosition / 10.0 - (CurrentPosition == 1 ? PositionX / 2500.0 : 0), 2);
+		}
+	}
+}
---
+++ Program/bin/Debug/net8.0-windows/MainWindow.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Program
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ObservableCollection<HorseRace> _horses = new();
+		private readonly Stopwatch _raceStopwatch = new();
+		private readonly DispatcherTimer _animationTimer = new();
+
+		private readonly List<string> _horsesNames = ["Lucky", "Ranger", "Willow", "Tucker", "Sabrina"];
+		private List<string> _activeHorsesNames = ["Lucky"];
+		private int _currentActiveHorseIndex;
+		
+		private int CurrentActiveHorseIndex 
+		{
+			get => _currentActiveHorseIndex;
+
+			set
+			{
+				if (value < 0) _currentActiveHorseIndex = _activeHorsesNames.Count - 1;
+				else if (value > _activeHorsesNames.Count - 1) _currentActiveHorseIndex = 0;
+				else _currentActiveHorseIndex = value;
+
+				OnCrtActiveHorseIndexChanging();
+			}
+		}
+
+		public string CurrentActiveHorse => _activeHorsesNames[_currentActiveHorseIndex];
+
+		public List<int> Bets { get; } = [10, 20, 50, 100, 200, 300, 500, 1000];
+		
+		private int _currentBetIndex = 0;
+
+		private int CurrentBetIndex
+		{
+			get => _currentBetIndex;
+
+			set
+			{
+				if (value < 0) _currentBetIndex = Bets.Count - 1;
+				else if (value > Bets.Count - 1) _currentBetIndex = 0;
+				else _currentBetIndex = value;
+
+				OnCrtBetIndexChanging();
+			}
+		}
+
+		private readonly List<Color> _jockeyColors = [Colors.Red, Colors.Blue, Colors.Green, Colors.Purple, Colors.Orange];
+
+		private int _finishedCount = 0;
+
+		private double _balance = 1000;
+
+		public double Balance 
+		{
+			get => _balance; 
+
+			set
+			{
+				if (value <= 0)
+				{
+					_balance = 0;
+					BetBtn.IsEnabled = false;
+				}
+				else _balance = value;
+
+				OnBalanceChanging();
+			}
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			InitializeAnimationTimer();
+			OnCrtBetIndexChanging();
+			Balance = 1000;
+		}
+
+		private void InitializeHorses()
+		{
+			var offsetY = 110;
+
+			var racetrackHeight = 120;
+
+			var numberOfHorses = int.Parse((NumberOfHorsesComboBox.SelectedItem as ComboBoxItem).Content.ToString());
+
+			var space = racetrackHeight / (numberOfHorses - 1);
+
+			for (var i = 0; i < numberOfHorses; i++)
+			{
+				var horse = new HorseRace(_horsesNames[i], _jockeyColors[i], 20, offsetY, val => Balance += val);
+
+				_horses.Add(horse);
+
+				offsetY += space;
+
+				var jockeyRectangle = new Rectangle
+				{
+					Width = horse.JockeyImage.Width,
+					Height = horse.JockeyImage.Height,
+					Fill = new SolidColorBrush(horse.Color),
+					OpacityMask = new ImageBrush
+					{
+						ImageSource = horse.JockeyImage.Source
+					}
+				};
+
+				RaceTrack.Children.Add(jockeyRectangle);
+
+				RaceTrack.Children.Add(horse.HorseImage);
+				Canvas.SetLeft(horse.HorseImage, horse.PositionX);
+				Canvas.SetTop(horse.HorseImage, horse.PositionY);
+				Canvas.SetLeft(jockeyRectangle, horse.PositionX);
+				Canvas.SetTop(jockeyRectangle, horse.PositionY - 30);
+			}
+		}
+
+		private void InitializeAnimationTimer()
+		{
+			_animationTimer.Interval = TimeSpan.FromMilliseconds(80);
+			_animationTimer.Tick += async (sender, e) => await UpdateHorsePositionsAsync();
+		}
+
+		private async Task UpdateHorsePositionsAsync()
+		{
+			var distanceTasks = _horses.Select(horse => horse.MoveAsync()).ToList();
+
+			 await Task.WhenAll(distanceTasks);
+
+			foreach (var horse in _horses)
+			{
+				Canvas.SetLeft(horse.HorseImage, horse.PositionX);
+				Canvas.SetTop(horse.HorseImage, horse.PositionY);
+				Canvas.SetLeft(horse.JockeyImage, horse.PositionX);
+				Canvas.SetTop(horse.JockeyImage, horse.PositionY - 30);
+				
+
+				if (horse.PositionX >= 600 && !horse.Finished)
+				{
+					horse.Time = _raceStopwatch.Elapsed;
+					_finishedCount++;
+					
+					if (_finishedCount >= _horses.Count - 1)
+					{
+						EndSimulation();
+						break;
+					}
+				}
+			}
+
+			_horses = new ObservableCollection<HorseRace>(_horses.OrderByDescending(horse => horse.PositionX));
+			HorsesDataGrid.ItemsSource = _horses;
+
+			for (var i = 0; i < _horses.Count; i++) 
+			{
+				_horses[i].CurrentPosition = i + 1;
+			}
+		}
+
+		private void Play_Button_Click(object sender, RoutedEventArgs e)
+		{
+			InitializeHorses();
+
+			_activeHorsesNames = _horses.Select(h => h.Name).ToList();
+			if (Balance >= 0) BetBtn.IsEnabled = Balance > 0;
+
+			_animationTimer.Start();
+			_raceStopwatch.Start();
+			
+			PlayPanel.Visibility = Visibility.Collapsed;
+			_finishedCount = 0;
+		}
+
+		private void EndSimulation()
+		{
+			_animationTimer.Stop();
+			_raceStopwatch.Stop();
+
+			PlayPanel.Visibility = Visibility.Visible;
+
+			_horses.Clear();
+			
+			for (int i = RaceTrack.Children.Count - 1; i >= 0; i--)
+			{
+				var child = RaceTrack.Children[i];
+				
+				if (child is FrameworkElement element && element.Tag as string == "tmp")
+				{
+					RaceTrack.Children.RemoveAt(i);
+				}
+			}
+		}
+
+		private void Previous_Bet_Btn_Click(object sender, RoutedEventArgs e) => CurrentBetIndex--;
+
+		private void Next_Bet_Btn_Click(object sender, RoutedEventArgs e) => CurrentBetIndex++;
+
+		private void Previous_Horse_Btn_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentActiveHorseIndex--;
+			if (_horses.Count != 0) BetBtn.IsEnabled = !_horses.First(h => h.Name == _activeHorsesNames[CurrentActiveHorseIndex]).IsBidClosed && Balance > 0;
+		}
+
+		private void Next_Horse_Btn_Click(object sender, RoutedEventArgs e) 
+		{
+			CurrentActiveHorseIndex++;
+			if (_horses.Count != 0) BetBtn.IsEnabled = !_horses.First(h => h.Name == _activeHorsesNames[CurrentActiveHorseIndex]).IsBidClosed && Balance > 0;
+		} 
+
+		private void BetBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Balance -= Bets[CurrentBetIndex];
+			if (_horses.Count != 0)
+			{
+				_horses.First(h => h.Name == _activeHorsesNames[CurrentActiveHorseIndex]).Money += Bets[CurrentBetIndex];
+				BetBtn.IsEnabled = false;
+			}
+		}
+			
+		private void OnCrtBetIndexChanging() => BetDisplay.Text = $"{Bets[CurrentBetIndex]}$";
+
+		private void OnBalanceChanging() => DisplayBalance.Text = $"Balance: {Math.Round(Balance, 2)}$";
+
+		private void OnCrtActiveHorseIndexChanging() => ActiveHorseNameDisplay.Text = _activeHorsesNames[CurrentActiveHorseIndex];
+
+	}
+}
